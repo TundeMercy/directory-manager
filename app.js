39e52240cb653a004df40bd20359ee2f8572e221
Tunde Ajagbe <tundeajagbea@gmail.com>
2019-08-25 11:27:31 +0100

    Some progress made :)

--- app.js
+++ app.js
@@ -1,26 +1,100 @@
 'use strict'
 
+// importing all dependencies
 const express = require('express');
-const directory = require('./directory');
+const Directory = require('./directory');
+const bodyParser = require('body-parser'); //middle-ware for parsing requests' body
+
 
 const app = express();
+
+//setting the port to listen on.
 const port = process.env.PORT || 5000;
 
-directory.addStaff({id: 1, name: 'Tunde Ajagbe', phone: 8106887783});
-directory.addStaff({id: 2, name: 'Tunde Ajagbe', phone: 8106887783});
-directory.addStaff({id: 3, name: 'Tunde Ajagbe', phone: 8106887783});
+//creating a new instance of Directory 
+const devC = new Directory();
 
+//add some staff to the created directory
+// for testing purpose.
+devC.addStaff({id: 1,
+    name: 'Tunde Ajagbe',
+    email: 'tundeajagbea@gmail.com',
+    phone: 8106887783});
+devC.addStaff({
+    id: 2,
+    name: 'John Akigbe',
+    email: 'johna@john.com',
+    phone: 8106887788});
+devC.addStaff({
+    id: 3,
+    name: 'Ayomikun Emmanuel',
+    email: 'Ayomikun@okun.com',
+    phone: 8106887783});
+
+//setting app to use body-parser for
+//proccessing the "body" of a request
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+//handle request to get the all the staff
+//in "this" directory.
+//Returns an array in JSON form
 app.get('/api/directory', function(req, res){
     res.status(200);
-    res.send(JSON.stringify(directory.contacts));
+    res.json(devC.getDirectory());
 
 });
 
+
+//handle the request to get a single staff
+//(with the specified :staffID) from "this" directory
+//Returns an object - JSON - if found
+//Otherwise, returns JSON with the key "error"
 app.get('/api/directory/:staffID', (req, res) => {
-    res.send(JSON.stringify(directory.getStaff(parseInt(req.params.staffID))));
+    const staff = devC.getStaff(req.params.staffID);
+    if(staff){
+        res.status(200);
+        res.json(staff);
+    } else {
+        res.status(404)
+        res.json({
+            error: "No such staff in our directory"
+        })
+    }
+    
 })
 
-//app.post('/api/directory/:staffID')
+//handle request to delete a staff
+//with the specified :staffID.
+//Return a JSON representation of the
+// deleted staff if found.
+//Otherwise, returns JSON with the key "error"
+app.delete('/api/directory/:staffID', (req, res) => {
+    const staffDeleted = devC.deleteStaff(req.params.staffID);
+    if(staffDeleted){
+        res.status(200);
+        res.json(staffDeleted);
+    } else{
+        res.status(404);
+        res.json(
+            {
+                error: "No such staff in our directory"
+            }
+        );
+    }
+});
+
+
+//handle post request to add a new staff
+//Returns a JSON representation of the
+//added staff
+app.post('/api/directory', (req, res) => {
+    const staffToAdd = req.body;
+    devC.addStaff(staffToAdd);
+    res.status(200);
+    res.json(devC.getStaff(staffToAdd.id));
+});
 
 app.listen(port, function(){
     console.log(`listening on port: ${port}`);
--- directory.js
+++ directory.js
@@ -1,6 +1,6 @@
 'use strict'
 
- module.exports = (function(){
+/*  module.exports = (function(){
     const contact = [];
 
     return {
@@ -10,14 +10,56 @@
         },
 
         getStaff: function(staffID){
-            /* const staff = this.contacts.find(function(staf){
-                staf.id === staffID;
+            const staff = this.contacts.find(function(staf){
+               return staf.id === staffID;
             });
 
             if(staff){ return staff;}
-            else {return "No such staff in our directory";} */ 
+            else {return "No such staff in our directory";}  
 
-            return this.contacts[staffID - 1];
         }
     };
-})();
+})(); */
+
+
+module.exports = class Directory{
+
+    // initialising a new instance with an empty
+    // array of staff.
+    constructor(){
+        this.directory = [];
+    }
+
+    
+    // returning an array of staff for a particular
+    // instance of Directory
+    getDirectory(){
+        return this.directory;
+    }
+
+    // adding a given staff to the main directory
+    // of the calling instance
+    addStaff(staff){
+        this.getDirectory().push(staff);
+    }
+
+    // returning a single staff with the specified staffID.
+    // returns "undefined" if no staff's id matches the
+    // provided staffID
+    getStaff(staffID){
+        const staff = this.directory.find(
+            s => { return s.id == staffID;});
+        if(staff) return staff;
+    }
+
+
+    // Deleting the staff with the specified staffID.
+    // Returns the deleted staff if found,
+    // otherwisw, returns "undefined".
+    deleteStaff(staffID){
+        const staff = this.directory.indexOf(
+            this.directory.find( s => { return s.id == staffID;}));
+        if(staff && staff !== -1) return this.directory.splice(staff, 1)[0];
+    }
+
+}
